bc9ddb05f49c5969daeab5243c3368f7bae6c164
GeorgeZhu <GeorgeZhu@10.0.0.9>
2016-08-31 19:32:35 -0600

    Changed greeting and banlance system

--- Craps/src/Craps.java
+++ Craps/src/Craps.java
@@ -1,4 +1,4 @@
-//test for Github
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Craps
@@ -8,26 +8,26 @@ public class Craps
         static int dice2;
         static int balance = 100;
         static int bet;
+        static String name; 
+        static boolean keepGoing = true;
 		public static void main(String[] args)
 			{
-				
-				Scanner userInput = new Scanner(System.in);
-			    System.out.println("What is your name?");
-			    String name = userInput.nextLine();
-			    System.out.println("Hello, " + name + "!");
+				greeting();				
 			    Scanner userInput2 = new Scanner(System.in);
 			    System.out.println("You have $100 now. How much money do you want to bet?");
 			    bet = userInput2.nextInt();
 			   
-			    rollDice();
+			    rollDice();			    
 			    if (sum == 7 || sum == 11) 
 			    	{
 			    System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + sum + ". Instant win, " + name + "! You have $" + win() + " now.");	   
 			    } else if (sum == 2 || sum == 12) 
 			    	{
 				    System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + sum + ". Too bad, " + name + ". Instant lose. You have $" + lose() + " now.");
-				    } 
-			    boolean keepGoing = true;
+				    boolean keepGoing = false;
+				} else {
+					System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + sum + ". Nothing happens, " + name + ". Let's reroll.");
+				}			    
 			    while( keepGoing ) {
 			    	rollDice();
 			         if (sum == 7){
@@ -42,9 +42,24 @@ public class Craps
 			        	 System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + sum + ". Reroll, " + name + ". You have $" + balance + " now.");
 			         }
 			      }
-			    
+			    System.out.println("You now have $" + balance + ", " + name + ".");
 			}
-		
+		public static void greeting(){
+			Calendar cal = Calendar.getInstance();
+			int hour = cal.get(Calendar.HOUR_OF_DAY);
+			String phrase;
+			if (hour < 12){
+				phrase = "Good morning";
+			} else if (hour < 17){
+				phrase = "Good afternoon";
+			} else {
+				phrase = "Good evening";
+			}
+			Scanner userInput = new Scanner(System.in);
+		    System.out.println(phrase + ", what is your name?");
+		    name = userInput.nextLine();
+		    System.out.println("Hello, " + name + "!");
+		}
 		public static int rollDice(){
 	         dice1 = (int)(Math.random()*6)+1;
 	         dice2 = (int)(Math.random()*6)+1;
